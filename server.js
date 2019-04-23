e9ccb1562b50bcd75b64c0007b25e1c768e780e6
GuilleAntico <antico.guillermo@gmail.com>
2019-04-23 19:47:54 -0300

    adding cluster to api

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 require('module-alias/register');
-
+const cluster = require('cluster');
+const os = require('os');
 const ServerService = require('./services/Server');
+const config = require('config');
 
+if(cluster.isMaster){
+    if(config.get('nodeEnv') === 'production'){
+        // Count the machine's CPUs
+        const cpuCount = os.cpus().length;
+    
+        // Create a worker for each CPU
+        for (let i = 0; i < cpuCount; i += 1) {
+            cluster.fork();
+        }
+    } else {
+        // Just fork once for dev
+        cluster.fork();
+    }
+} else {
+    ServerService.start();
+}
 
-ServerService.start();
 
