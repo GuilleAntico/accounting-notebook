c4ffed8d1d6e8a2721a6bcac483530400509be83
GuilleAntico <antico.guillermo@gmail.com>
2019-04-26 19:12:50 -0300

    adding build folder

---
+++ front/build/precache-manifest.e09913050060de7347d5a482593a0c18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e2baac53636d87be01f63613f3241e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a539f297a72cb008ce52",
+    "url": "/static/css/main.1e808095.chunk.css"
+  },
+  {
+    "revision": "2ff626c471b3c60217eb",
+    "url": "/static/js/2.2576f55c.chunk.js"
+  },
+  {
+    "revision": "a539f297a72cb008ce52",
+    "url": "/static/js/main.defa36d6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
